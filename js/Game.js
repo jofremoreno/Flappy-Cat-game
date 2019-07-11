01fb9e8fe7f8bee0866ccd63cf2782da98374cb2
Jofre Moreno <jofremoreno@gmail.com>
2019-07-11 22:50:57 +0200

    add colisión del player con obstaculos superiores

--- js/Game.js
+++ js/Game.js
@@ -21,45 +21,62 @@ class Game {
     this.ctx.fillRect(0, 0, 300, 300);
   }
   start() {
+    setInterval(() => {
+      let height = 0; //para el mathrandom
+      arrayObstacle.push(
+        new Obstaculos(500, 0, 40, 130, "rgba(150,150,0,1)", 1)
+      );
+      arrayObstacle.push(
+        new Obstaculos(500, 300 - 40, 40, 100, "rgba(150,150,0,1)", 1)
+      );
+      if (arrayObstacle.length > 8) {
+        arrayObstacle.shift();
+        arrayObstacle.shift();
+      }
+    }, 2500);
     this.loopGame = window.requestAnimationFrame(this._update.bind(this));
   }
   _update() {
     this.ctx.fillStyle = "rgba(0,0,0,1)";
     this.ctx.fillRect(0, 0, 500, 300); //borrado
     this.player.draw();
-    this.obstacle.update();
-    this.obstacle2.update();
 
-    var resultado = this.player.update(300, 500, 0, 0);
-    this.obstacle.draw();
-    this.obstacle2.draw();
-    if (this.checkCollisionUp(this.obstacle)) {
-      console.log("he colisionado");
-      resultado = -1;
-    }
-    if (resultado == -1) {
+    arrayObstacle.forEach(obstacle => {
+      obstacle.update();
+      obstacle.draw();
+      if (this.checkCollisionUp(obstacle) === true) {
+        console.log("he colisionado");
+        resultado = -1;
+      }
+    });
+    if (resultado === -1) {
+      window.cancelAnimationFrame(this.loopGame);
     } else {
       this.loopGame = window.requestAnimationFrame(this._update.bind(this));
     }
+    console.log("array");
+    var resultado = this.player.update(300, 500, 0, 0);
+
   }
+  //COLISION SUPERIOR
   checkCollisionUp(obstacle) {
     console.log("dentro CheckCollision");
+    var myLeft = this.player.x;
+    var myRight = this.player.x + this.player.width;
+    var myTop = this.player.y;
+    var myBottom = this.player.y + this.player.height;
 
-    if (
-      (this.player.x + this.player.width > obstacle.x && this.player.x + this.player.width < obstacle.x &&
-        this.player.y < obstacle.y + obstacle.height) ||
-      (this.player.x + this.player.width > this.obstacle2.x && this.player.x + this.player.width < this.obstacle2.x &&
-        this.player.y + this.player.height > this.obstacle2.y)
-    ) {
-      return true;
-    } else {
-      false;
-    }
+    var obsLeft = obstacle.x;
+    var obsRight = obstacle.x + obstacle.width;
+    var obsTop = obstacle.y;
+    var obsBottom = obstacle.y + obstacle.height;
+
+    var crash = true;
+
+    if ( myBottom < obsTop || myTop > obsBottom || myRight < obsLeft || myLeft > obsRight ) {
+    crash = false;
   }
-  // if (this.player.x + this.player.width > this.obstacle.x
-  // && this.player.y < this.obstacle.y + this.obstacle.height) {
-  // return true
-  // }else{
-  // return false
-  // }
-}
+  return crash;
+};
+
+  }
