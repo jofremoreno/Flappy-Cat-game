a787c44fdf8375d48a715e00af5ff15fd72949c1
Jofre Moreno <jofremoreno@gmail.com>
2019-07-13 19:16:12 +0200

    add botón para iniciar juego

--- js/run.js
+++ js/run.js
@@ -1,11 +1,21 @@
 var game = new Game();
-game.initCanvas();
-game.start();
 var arrayObstacle = [];
-window.addEventListener("keyup", function(e) {
-  if (e.keyCode == 32) {
-    console.log("espacio");
-    game.player.jumpp();
-    console.log(game.player.y);
-  }
-});
+document.onload = (function() {
+  var mybutton = document.getElementById("myButton");
+  mybutton.addEventListener("click", () => {
+    game.initCanvas();
+    game.start();
+    mybutton.style.display="none"
+
+    window.addEventListener("keyup", function(e) {
+      if (e.keyCode == 32) {
+        console.log("espacio");
+        game.player.jumpp();
+        console.log(game.player.y);
+      }
+    });
+  });
+  console.log(mybutton);
+})();
+
+
