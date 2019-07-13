7213c338993d2c5b958875c589e149535ae64c5b
Jofre Moreno <jofremoreno@gmail.com>
2019-07-13 12:25:58 +0200

    delete algunos comentarios

--- js/Obstaculo.js
+++ js/Obstaculo.js
@@ -5,7 +5,6 @@ class Obstaculos {
     this.width = width;
     this.height = height;
     this.color = color;
-    //intentando mantener espaciado entre obstaculos siempre iguales
   }
   draw() {
     game.ctx.fillStyle = this.color;
